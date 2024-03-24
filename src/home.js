1344d93d7ec0aa55fb030c16cd9be796e5547622
shenmadu <tashen.madu@gmail.com>
2024-03-24 23:45:38 +0530

    load todolist table

--- src/home.js
+++ src/home.js
@@ -1,5 +1,31 @@
-function taskAdd(){
-    const task=document.getElementById("inputTask").value
-    
-    
+const todoList = document.getElementById('task-list');
+const todoInput = document.getElementById("inputTask")
+
+function taskAdd() {
+    const task = document.getElementById("inputTask").value
+    const li = document.createElement('li');
+
+
+    const doneBtn = document.createElement("button");
+    doneBtn.textContent = "Done";
+    doneBtn.classList.add("done-btn")
+
+    doneBtn.addEventListener('click', function () {
+        li.classList.toggle("completed")
+
+    })
+   
+    doneBtn.addEventListener('click', ()=> {
+        li.remove();
+    });
+
+
+
+    li.textContent = task;
+    li.appendChild(doneBtn);
+    todoList.appendChild(li);
+    todoInput.value = "";
+
+
+
 };
